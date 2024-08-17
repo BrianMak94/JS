4af80610114aa058bed6e48e65a5956ff47d35e6
BrianMak94 <130622976+BrianMak94@users.noreply.github.com>
2024-08-18 04:25:08 +0800

    Update No_img.js

--- No_img.js
+++ No_img.js
@@ -1,13 +1,27 @@
-// Function to block all images
-function blockAllImages() {
-    // Get all image elements
-    const images = document.querySelectorAll('img');
-    
-    // Hide each image
-    images.forEach(image => {
-        image.style.display = 'none';
-    });
-}
+// ==UserScript==
+// @name         Block All Images
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Block all images on the webpage by hiding them
+// @author       You
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
 
-// Run the function to block images
-blockAllImages();
+(function() {
+    'use strict';
+
+    // Function to block all images
+    function blockAllImages() {
+        // Get all image elements
+        const images = document.querySelectorAll('img');
+
+        // Hide each image
+        images.forEach(image => {
+            image.style.display = 'none';
+        });
+    }
+
+    // Run the function to block images
+    blockAllImages();
+})();
