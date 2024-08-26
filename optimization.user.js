299ef03b4298e39cde68ddbe4c5c7eaff9dd397a
BrianMak94 <130622976+BrianMak94@users.noreply.github.com>
2024-08-26 13:41:42 +0800

    Update optimization.user.js

--- optimization.user.js
+++ optimization.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Enhanced Page Optimizer
 // @namespace    http://tampermonkey.net/
-// @version      1.5
+// @version      1.5.1
 // @description  Optimizes page by applying various performance improvements
 // @match        *://*/*
 // @grant        none
